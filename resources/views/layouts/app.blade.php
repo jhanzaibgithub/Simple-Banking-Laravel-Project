24cc7d6aa376b8859fd7e68e1728440e71926dff
jahanzaib sarri <124418010+jhanzaibgithub@users.noreply.github.com>
2024-09-22 09:56:39 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,16 +42,7 @@
             </div>
         @endif
 
-        @if($errors->any())
-            <div class="alert alert-danger mt-3">
-                <ul class="mb-0">
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
+      
         @yield('content')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
