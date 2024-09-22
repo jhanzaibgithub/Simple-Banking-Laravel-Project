f0a8e7018c005918fa165bba61e0e6a124746b5b
jahanzaib sarri <124418010+jhanzaibgithub@users.noreply.github.com>
2024-09-22 09:04:36 +0500

    Update header.blade.php

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -26,23 +26,6 @@
 <body>
 
 
-
-        @if(session('success'))
-            <div class="alert alert-success mt-3">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        @if($errors->any())
-            <div class="alert alert-danger mt-3">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         @yield('content')
 
 
