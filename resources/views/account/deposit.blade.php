d204197cac376bed3299fbcc4dc39e2b1adf91e2
jahanzaib sarri <124418010+jhanzaibgithub@users.noreply.github.com>
2024-09-22 09:52:45 +0500

    Update deposit.blade.php

--- resources/views/account/deposit.blade.php
+++ resources/views/account/deposit.blade.php
@@ -9,21 +9,15 @@
         </div>
         <div class="card-body">
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
     <form action="{{ route('deposit') }}" method="POST">
         @csrf
