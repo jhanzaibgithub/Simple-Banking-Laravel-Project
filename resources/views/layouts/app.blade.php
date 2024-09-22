49e03412b26b8cc02ba57b9cc598290a35f59eed
jahanzaib sarri <124418010+jhanzaibgithub@users.noreply.github.com>
2024-09-22 12:51:53 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,12 @@
     <title>ABC Bank Statement</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+      <style>
+        body {
+            background-color: #b0bec5;
+}
+
+    </style>
 </head>
 <body>
 
